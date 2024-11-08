f9260eeae4b3c806a126044b45db31ee9373993c
cyprich <cypooriginal@gmail.com>
2024-11-08 21:10:38 +0100

    created new react typescript tailwind app

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+function App() {
+	return (
+		<>
+			<h1>Minecraft Server</h1>
+			<p>Vitaj na našom úžasnom Minecraft Serveríku!</p>
+			<p>Na stránke sa momentálne pracuje...</p>
+			<br/>
+			<p>Sme traja skibidiózni hráči</p>
+			<ul>
+				<li>YungCypo</li>
+				<li>Adushkaaa</li>
+				<li>Pacholicek2810</li>
+			</ul>
+		</>
+	)
+}
+
+export default App
